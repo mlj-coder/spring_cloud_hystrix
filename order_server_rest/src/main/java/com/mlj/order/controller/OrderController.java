c08627b1b7e385ff17d1248995774a7d3e3c5458
mlj-coder <2925214095@qq.com>
2022-09-05 18:32:31 +0800

    使用hystrix对rest简单使用

---
+++ order_server_rest/src/main/java/com/mlj/order/controller/OrderController.java
@@ -0,0 +1,46 @@
+package com.mlj.order.controller;
+
+import com.mlj.product.entity.Product;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+
+    //注入rest Template对象
+    @Autowired
+    private RestTemplate restTemplate;
+
+    /**
+     * 使用注解配置熔断保护
+     *      fallbackmethod:配置熔断之后的降级方法
+     */
+    @HystrixCommand(fallbackMethod = "orderFallBack")
+    @RequestMapping(value = "/buy/{id}",method = RequestMethod.GET)
+    public Product findById(@PathVariable Long id){
+        Product forObject = restTemplate.getForObject("http://service-product/product/"+id, Product.class);
+        return forObject;
+    }
+
+    /**
+     * 降级方法
+     *  和需要收到保护的方法的返回值一致
+     */
+    public Product orderFallBack(Long id){
+        Product forObject = new Product();
+        forObject.setProductName("触发降级方法");
+        return forObject;
+    }
+
+}
